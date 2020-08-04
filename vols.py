0b46493b53cb93b065c69f9ceca62593b29cb774
Machendra <machendra.surya@gmail.com>
2020-08-04 03:02:35 +0000

    modified EBS volumes script

--- vols.py
+++ vols.py
@@ -1,5 +1,6 @@
 import os
 import json
+import sys
 #collecting json data
 json_data=os.popen("aws ec2 describe-volumes").read()
 #converrting json data into dict
@@ -7,10 +8,11 @@ dict_data=json.loads(str(json_data))
 #opening a file
 myfile=open("my_server_info.txt","w")
 myfile.writelines("s.no\tvol_id\tsize\n")
-#Appending volume data in to file 
-#myfile=open("my_server_info.txt","a")
-for i in range(len(dict_data["Volumes"])):
-   # print >> myfile,i+1,"\t",dict_data["Volumes"][i]['VolumeId'],dict_data["Volumes"][i]['Size']
-
-    print (i+1,"\t",dict_data["Volumes"][i]['VolumeId'],dict_data["Volumes"][i]['Size'])
 myfile.close()
+#Appending volume data in to file 
+sys.stdout=open("my_server_info.txt","a")
+for i in range(len(dict_data["Volumes"])):
+#	print >> myfile,i+1,"\t",dict_data["Volumes"][i]['VolumeId'],dict_data["Volumes"][i]['Size']
+    print (i+1,"\t",dict_data["Volumes"][i]['VolumeId'],dict_data["Volumes"][i]['Size'])
+#	print (i+1\tdict_data["Volumes"][i]['VolumeId']\t\tdict_data["Volumes"][i]['Size'])
+sys.stdout.close()
